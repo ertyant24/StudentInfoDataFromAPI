2e0093e93a814f8f0fb3e8a17fbd9ac722ab4f60
ertyant24 <erikciertugrul@gmail.com>
2023-01-25 18:18:27 +0300

    Add project files.

---
+++ StudentInfoDataFromAPI/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestSharp;
+using StudentInfoDataFromAPI.Models;
+using System.Diagnostics;
+using System.Net;
+using System.Collections.Generic;
+
+namespace StudentInfoDataFromAPI.Controllers
+{
+	public class HomeController : Controller
+	{
+		[HttpGet]
+		public IActionResult Index()
+		{
+
+			RestClient client = new RestClient("http://localhost:5156/api");
+			RestRequest request = new RestRequest("/Student", Method.Get);
+			List<Student> response = client.Get<List<Student>>(request);
+
+			return View(response);
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
---
+++ StudentInfoDataFromAPI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StudentInfoDataFromAPI.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
