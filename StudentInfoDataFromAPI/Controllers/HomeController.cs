e8032951453d375b3c6534ffacc3ae89e1ba1a2b
ertyant24 <erikciertugrul@gmail.com>
2023-01-26 13:49:11 +0300

    Added => Edit, Delete

--- StudentInfoDataFromAPI/Controllers/HomeController.cs
+++ StudentInfoDataFromAPI/Controllers/HomeController.cs
@@ -36,7 +36,54 @@ namespace StudentInfoDataFromAPI.Controllers
 			return RedirectToAction(nameof(Index));
 		}
 
+		[HttpGet]
+		public IActionResult Edit(int id)
+		{
 
+			RestClient client = new RestClient("http://localhost:5156/api/Student");
+			RestRequest request = new RestRequest($"/update/{id}", Method.Get);
+			RestResponse<Student> student = client.ExecuteGet<Student>(request);
+
+			return View(student);
+		}
+
+		public IActionResult Edit(int id, UpdateStudentModel model)
+		{
+
+			RestClient client = new RestClient("http://localhost:5156/api/Student");
+			RestRequest request = new RestRequest($"/update/{id}", Method.Put);
+			request.AddJsonBody(model);
+			RestResponse<Student> response = client.ExecutePost<Student>(request);
+
+			if(response.IsSuccessful)
+			{
+				return RedirectToAction(nameof(Index));
+			}
+			else
+			{
+				ModelState.AddModelError("", "Student is Not Found.");
+				return View(model);
+			}
+		}
+
+		public IActionResult Delete(int id)
+		{
+
+			RestClient client = new RestClient("http://localhost:5156/api/Student");
+			RestRequest request = new RestRequest($"/delete/{id}", Method.Delete);
+			RestResponse response = client.Delete(request);
+
+			if (response.IsSuccessful)
+			{
+				TempData["success"] = "Student is Deleted.";
+			}
+			else
+			{
+				TempData["success"] = "Student is not Deleted.";
+			}
+			
+			return RedirectToAction(nameof(Index));
+		}
 
 		public IActionResult Privacy()
 		{
---
+++ StudentInfoDataFromAPI/Models/UpdateStudentModel.cs
@@ -0,0 +1,11 @@
+﻿namespace StudentInfoDataFromAPI.Models
+{
+	public class UpdateStudentModel
+	{
+		public string FullName { get; set; }
+		public string College { get; set; }
+		public int Age { get; set; }
+		public string Computer { get; set; }
+		public bool IsSuccess { get; set; }
+	}
+}
